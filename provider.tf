49268b2653fed1bcfa1c94207a90f597077081d3
Midhunts1 <88725292+Midhunts1@users.noreply.github.com>
2021-09-30 16:51:45 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
-    credentials = file("my_project.json")
-    project = "my-project-devops2021"
-    region = "asia-south1"
+  project     = "eur-devops"
+  #credentials = "${file("eur-devops.json")}"
+  region      = "us-central1"
 }
